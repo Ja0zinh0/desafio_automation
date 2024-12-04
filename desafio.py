229fe8b13d685f38186fef9c5fcc77c6966f5c44
João Silva <163667621+Ja0zinh0@users.noreply.github.com>
2024-12-04 14:13:09 -0300

    Create desafio.py

---
+++ desafio.py
@@ -0,0 +1,8 @@
+import pyautogui; from mouseinfo import mouseInfo
+#criação de pastas automaticamente 
+
+
+pyautogui.rightClick(856,229,duration=2)
+pyautogui.move(0,220,duration=2)
+pyautogui.move(-250,0,duration=2)
+pyautogui.click()
